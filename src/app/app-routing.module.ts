5dff184db951a0e296cab4f104bab2c92ed8d355
AnthonyMoortgat <44052663+AnthonyMoortgat@users.noreply.github.com>
2018-10-23 23:29:26 +0200

    Template project. With sidebar and module components added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IdeaboxComponent } from './ideabox/ideabox.component';
+import { MemberlistComponent } from './memberlist/memberlist.component';
+import { DebtlistComponent } from './debtlist/debtlist.component';
+import { UsersettingsComponent } from './usersettings/usersettings.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersettingsComponent
+  },
+  {
+    path: 'memberlist',
+    component: MemberlistComponent
+  },
+  {
+    path: 'debtlist',
+    component: DebtlistComponent
+  },
+  {
+    path: 'ideabox',
+    component: IdeaboxComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { MemberlistComponent } from './memberlist/memberlist.component';
+import { IdeaboxComponent } from './ideabox/ideabox.component';
+import { DebtlistComponent } from './debtlist/debtlist.component';
+import { UsersettingsComponent } from './usersettings/usersettings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    MemberlistComponent,
+    IdeaboxComponent,
+    DebtlistComponent,
+    UsersettingsComponent
   ],
   imports: [
     BrowserModule,

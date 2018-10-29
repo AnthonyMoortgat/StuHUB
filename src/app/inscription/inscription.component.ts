505d323eec03324d7e9e0e95421e611b0a4673b7
AnthonyMoortgat <44052663+AnthonyMoortgat@users.noreply.github.com>
2018-10-29 17:32:14 +0100

    Creating a template for the app + angular form

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-inscription',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InscriptionComponent implements OnInit {
 
-  constructor() { }
+  messageForm: FormGroup;
+  submitted = false;
+  success = false;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.messageForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      allergy: ['', Validators.required],
+      physicalLimitation: ['', Validators.required],
+      birthdate: ['', Validators.required],
+      gender: ['', Validators.required],
+      email: ['', Validators.required]
+    });
   }
 
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.messageForm.invalid) {
+      return;
+    }
+
+    this.success = true;
+  }
 }

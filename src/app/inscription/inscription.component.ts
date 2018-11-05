2d6089b74bc547cbe0df5ec5f1bbb9d281fcadd3
AnthonyMoortgat <44052663+AnthonyMoortgat@users.noreply.github.com>
2018-11-05 21:04:07 +0100

    Trying to connect with the database using php and ts. (ERR_CONNECTION_REFUSED) errorr when i look at the console.

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,49 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { Inscription } from './inscription';
+import { InscriptionService } from '../inscription.service';
+
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
   styleUrls: ['./inscription.component.scss']
 })
 export class InscriptionComponent implements OnInit {
+  /* inscription */
+  inscription: Inscription[];
+  error = '';
+  success = '';
 
-  messageForm: FormGroup;
-  submitted = false;
-  success = false;
-
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit() {
-    this.messageForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      lastName: ['', Validators.required],
-      phoneNumber: ['', Validators.required],
-      allergy: ['', Validators.required],
-      physicalLimitation: ['', Validators.required],
-      birthdate: ['', Validators.required],
-      gender: ['', Validators.required],
-      email: ['', Validators.required]
-    });
+  constructor(private inscriptionService: InscriptionService) {
   }
 
-  onSubmit() {
-    this.submitted = true;
+  ngOnInit() {
+    this.getInscription();
+  }
 
-    if (this.messageForm.invalid) {
-      return;
-    }
-
-
-    const name: string = this.messageForm.get('name').value;
-    const lastName: string = this.messageForm.get('lastName').value;
-    const phoneNumber: string = this.messageForm.get('phoneNumber').value;
-    const allergy: string = this.messageForm.get('allergy').value;
-    const physicalLimitation: string = this.messageForm.get('physicalLimitation').value;
-    const birthdate: string = this.messageForm.get('birthdate').value;
-    const gender: string = this.messageForm.get('gender').value;
-    const email: string = this.messageForm.get('email').value;
-
-    this.success = true;
+  getInscription(): void {
+    this.inscriptionService.getAll().subscribe(
+      (res: Inscription[]) => {
+        this.inscription = res;
+      },
+      (err) => {
+        this.error = err;
+      }
+    );
   }
 }

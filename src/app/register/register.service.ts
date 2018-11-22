69a7358ed802bbd72fc9c7b6e9081ee263f5fb96
Michael De Gauquier <michael.de.gauquier@student.ehb.be>
2018-11-22 10:48:28 +0100

    login and register 22/11/18

---
+++ src/app/register/register.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+import { User } from './register';
+import {NULL_INJECTOR} from '@angular/core/src/render3/component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  baseUrl = 'http://dtsl.ehb.be/~michael.de.gauquier/SP2/api';
+  registers: User[];
+
+  constructor(private http: HttpClient) { }
+  /*
+  getAll(): Observable<User[]> {
+    return this.http.get(`${this.baseUrl}/registerList.php`).pipe(
+      map((res) => {
+        this.registers = res['data'];
+        return this.registers;
+      }),
+      catchError(this.handleError));
+  }*/
+
+  store(register: User): Observable<User[]> {
+    return this.http.post(`${this.baseUrl}/registerStore.php`, { data: register })
+      .pipe(map((res) => {
+          this.registers.push(res['data']);
+          return this.registers;
+        }),
+        catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log(error);
+
+    // return an observable with a user friendly message
+    return throwError('Error! something went wrong.');
+  }
+}
